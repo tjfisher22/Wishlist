536f2165f9f4bded49355ffbb6eec7077b9aebb6
Thomas Fisher <tfis113@wgu.edu>
2022-12-11 12:28:53 -0700

    Create Query to Add Person ID to Persons Table

--- Population.sql
+++ Population.sql
@@ -5,13 +5,24 @@ VALUES	('Ron', 'Password1',1),
 		('Kelsey','Password4',1),
 		('Daniel','Password5',1),
 		('Jarett','Password6',1),
+		('Charlotte','Password7',1),
 		('Matthew','Password8',1)
 
 INSERT INTO Presents (GiftName, GiftDescription, GiftURL, Purchased)
 SELECT GiftName, GiftDescription, GiftURL, Purchased
 FROM CSVExport
- 
-INSERT INTO PersonsPresent
 
-SELECT PesentID, PresentName, PersonID, PersonName
-FROM  
+UPDATE Presents
+SET Presents.PersonID = Temp.PersonID
+FROM Presents
+INNER JOIN (
+	SELECT PersonID, GiftName
+	FROM  CSVExport
+	INNER JOIN Persons ON Person = PersonName) AS Temp ON Temp.GiftName = Presents.GiftName
+
+SELECT Presents.GiftName, Presents.PersonID, Persons.PersonName
+FROM Presents
+INNER JOIN Persons ON Presents.PersonID = Persons.PersonID
+
+
+
